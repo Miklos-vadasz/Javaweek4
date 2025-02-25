a0b7add285d1634a893075c546ba4a13dbf2afc4
Miklos-vadasz <mvadasz@stu.socsd.org>
2025-02-25 10:29:21 -0500

    Create Digits.java

---
+++ Digits.java
@@ -0,0 +1,20 @@
+/*2/25/25, Miklos Vadasz Digets*/
+
+public class Digits{
+
+public static void main(String[]args){
+
+    int number;
+    number = 258;
+    int ones;
+    int tens;
+    int hundreds;
+    ones = number % 10;
+    tens = (number / 10) % 10;
+    hundreds = (number / 10 / 10) % 10;
+    
+    System.out.println(ones);
+    System.out.println(tens);
+    System.out.println(hundreds);
+}
+ }
