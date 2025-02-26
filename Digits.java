c09a0a68524ab377ee237ba00c087ecdcf1ec713
Miklos-vadasz <mvadasz@stu.socsd.org>
2025-02-26 10:26:32 -0500

    Update Digits.java

--- Digits.java
+++ Digits.java
@@ -1,20 +1,38 @@
 /*2/25/25, Miklos Vadasz Digets*/
 
+import java.util.*;
+
 public class Digits{
 
 public static void main(String[]args){
 
     int number;
-    number = 258;
+    /*number = 2584;*/
     int ones;
     int tens;
     int hundreds;
+    int thousands;
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a 4 digits number");
+    number = input.nextInt();
+
     ones = number % 10;
     tens = (number / 10) % 10;
     hundreds = (number / 10 / 10) % 10;
+    thousands = (number / 10 / 10 / 10) % 10;
     
-    System.out.println(ones);
-    System.out.println(tens);
+
+    System.out.println(thousands);
     System.out.println(hundreds);
+    System.out.println(tens);
+    System.out.println(ones);
+
+    System.out.println("The thousands digit is " + thousands);
+    System.out.println("The hundreds digit is " + hundreds);
+    System.out.println("The tens digit is " + tens);
+    System.out.println("The ones digit is " + ones);
+    
+    
 }
  }
