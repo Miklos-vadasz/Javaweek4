9048ab6150119bf55c4bbfa64f9a9e7107784635
Miklos-vadasz <mvadasz@stu.socsd.org>
2025-03-06 10:30:31 -0500

    Sbrgh

---
+++ Sleep.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Sleep {
+
+    public static void main(String[]args){
+
+        Scanner input = new Scanner(System.in);
+
+        int birthyear;
+        int birthmonth;
+        int birthday;
+        int todayear;
+        int month;
+        int day;
+        int timealive;
+        int hourslept;
+        int monthalive;
+        int dayalive;
+
+
+        System.out.print("Enter birthyear: ");
+        birthyear = input.nextInt();
+        System.out.print("Enter birthmonth : ");
+        birthmonth = input.nextInt();
+        System.out.print("Enter birthday : ");
+        birthday = input.nextInt();
+        System.out.print("Enter todayear: ");
+        todayear = input.nextInt();
+        System.out.print("Enter month : ");
+        month = input.nextInt();
+        System.out.print("Enter day : ");
+        day = input.nextInt();
+
+        timealive = (todayear - birthyear) * 365;
+        monthalive = () 
+
+
+    }
+    
+    
+}
