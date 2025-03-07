fe0d85d6b6a85ad3a99d367c222605657d5eed43
Miklos-vadasz <mvadasz@stu.socsd.org>
2025-03-07 10:28:06 -0500

    xdrh

--- Sleep.java
+++ Sleep.java
@@ -1,40 +1,55 @@
 import java.util.*;
+import java.text.NumberFormat;
+
+
 public class Sleep {
-
-    public static void main(String[]args){
-
-        Scanner input = new Scanner(System.in);
-
-        int birthyear;
-        int birthmonth;
-        int birthday;
-        int todayear;
+    public static void main(String[]args){  
+        int birthdate;
+        int bdyear;
+        int bdmonth;
+        int bdday;
+        int year;
         int month;
         int day;
-        int timealive;
-        int hourslept;
-        int monthalive;
-        int dayalive;
+        int daysalive;
+        int sleep;
+    
+        Scanner input = new Scanner(System.in);
+        NumberFormat number = NumberFormat.getIntegerInstance();
+        System.out.println("Enter your birthdate: ");
+        System.out.println("Year: ");
+        bdyear = input.nextInt();
+        System.out.println("Month: ");
+        bdmonth = input.nextInt();
+        System.out.println("Day: ");
+        bdday = input.nextInt();
 
 
-        System.out.print("Enter birthyear: ");
-        birthyear = input.nextInt();
-        System.out.print("Enter birthmonth : ");
-        birthmonth = input.nextInt();
-        System.out.print("Enter birthday : ");
-        birthday = input.nextInt();
-        System.out.print("Enter todayear: ");
-        todayear = input.nextInt();
-        System.out.print("Enter month : ");
+        System.out.println("Enter today's date: ");
+        System.out.println("Year: ");
+        year = input.nextInt();
+        System.out.println("Month: ");
         month = input.nextInt();
-        System.out.print("Enter day : ");
+        System.out.println("Day: ");
         day = input.nextInt();
 
-        timealive = (todayear - birthyear) * 365;
-        monthalive = () 
+
+        daysalive = ((year - bdyear) * 365) + ((month - bdmonth)* 30) + (day - bdday);
+        sleep = (daysalive * 8);
+
+
+        System.out.println ("You have been alive for " + number.format(daysalive) + " days");
+        System.out.println("You have slept " + number.format(sleep) + " hours");    
+    
 
 
     }
     
+
+
+
+
+
+
     
 }
